581793f62410e4a9d99b5ef10790503b6201f3ba
Parv Khandelwal <parv10046@gmail.com>
2026-04-20 12:58:10 +0530

    Initial commit

---
+++ src/test/java/com/example/core/api/guardrails/CoreApiGuardrailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.core.api.guardrails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApiGuardrailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
