49690e8c337d4ed688a700ccfd4f8e98eba94546
Parv Khandelwal <parv10046@gmail.com>
2026-04-24 22:21:41 +0530

    Refactor NotificationService to optimize pending notification retrieval using Redis scan and improve error handling

--- src/main/java/com/example/core/api/guardrails/service/NotificationService.java
+++ src/main/java/com/example/core/api/guardrails/service/NotificationService.java
@@ -2,10 +2,15 @@ package com.example.core.api.guardrails.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.Cursor;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.awt.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Service
@@ -18,29 +23,24 @@ public class NotificationService {
     public void notificationSchedule() {
         log.info("Notification Service is running...");
 
-        Set<String> keys = redisTemplate.keys("user:*:pending");
-
-        if (keys == null || keys.isEmpty()) {
-            log.info("No pending notifications found");
-            return;
-        }
-
-        for (String key : keys) {
-            Long count = redisTemplate.opsForList().size(key);
-            if (count!=null && count > 0) {
-                String firstMessage = redisTemplate.opsForList().index(key, 0);
-                if (count == 1) {
-                    log.info("Push Notification Sent to User: {}", firstMessage);
-                } else {
-                    long otherCount = count - 1;
-                    log.info("Summarized Push Notification: {} and {} others interacted with your posts.", firstMessage, otherCount);
-                }
-                redisTemplate.delete(key);
-
-
-            }
-
-        }
+        Set<String> keys=pendingKeys();
 
     }
+    private Set<String> pendingKeys(){
+        Set<String> keys=new HashSet<>();
+        ScanOptions options=ScanOptions.scanOptions().match("user:*:pening").build();
+        redisTemplate.execute((RedisCallback<Void>)connection->{
+            try(Cursor<byte[]> cursor=connection.scan(options)) {
+                while(cursor.hasNext()) {
+                    keys.add(new String(cursor.next()));
+                }
+            }catch(Exception e){
+                log.error("error scanning redis ",e);
+            }
+            return null;
+
+        });
+        return keys;
+    }
+
 }
